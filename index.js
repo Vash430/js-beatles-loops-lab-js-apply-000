814227b7091d42ed32ec511854bb76b332843c68
essential-form-0157 <vasmith430@gmail.com>
2020-07-04 13:07:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments) {
+  var array = [ ];
+  for (var i = 0; i < musicians.length; i++){
+    array.push(musicians[i] + 'plays' + instruments[i]);
+  }
+  return array;
+}
+
+function johnLennonFacts ( ) {
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegeterian", "He was a choir boy and boy scout", "He hated the sound of his own voice" ];
+  var array = [ ];
+  var i = 0;
+  while (array.lengh < facts.length) {
+    array.push(facts[i] + '!!!');
+    i++;
+  }
+  return array;
+}
+
+function iLoveTheBeatles (number){
+  var arr = [ ];
+  var i = 0;
+  do {
+    arr.push('I love the Beatles!');
+    i++;
+  }
+  while (i <= number && number < 15);
+  return arr;
+}
